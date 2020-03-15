20b094db1d1b7b658b175bb23d39b23a76544a18
Romanos Skiadas <rski@arista.com>
2020-03-15 19:29:25 +0000

    change hostname and TZ

--- configuration.nix
+++ configuration.nix
@@ -14,7 +14,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  # networking.hostName = "nixos"; # Define your hostname.
+  networking.hostName = "belauensis";
 
   # The global useDHCP flag is deprecated, therefore explicitly set to false here.
   # Per-interface useDHCP will be mandatory in the future, so this generated config
@@ -32,7 +32,7 @@
   # };
 
   # Set your time zone.
-  # time.timeZone = "Europe/Amsterdam";
+  time.timeZone = "Europe/Dublin";
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
