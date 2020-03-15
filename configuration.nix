7e5978b09621030d6e5a34b99885b987d5bc24cb
Romanos Skiadas <rski@arista.com>
2020-03-15 19:05:53 +0000

    add laptop config

---
+++ configuration.nix
@@ -0,0 +1,113 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # networking.hostName = "nixos"; # Define your hostname.
+
+  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
+  # Per-interface useDHCP will be mandatory in the future, so this generated config
+  # replicates the default behaviour.
+  networking.useDHCP = false;
+  networking.interfaces.enp0s31f6.useDHCP = true;
+  networking.interfaces.wlp4s0.useDHCP = true;
+  networking.interfaces.wwp0s20f0u6i12.useDHCP = true;
+
+  # Select internationalisation properties.
+  # i18n = {
+  #   consoleFont = "Lat2-Terminus16";
+  #   consoleKeyMap = "us";
+  #   defaultLocale = "en_US.UTF-8";
+  # };
+
+  # Set your time zone.
+  # time.timeZone = "Europe/Amsterdam";
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  nixpkgs.config.allowUnfree = true;
+  environment.systemPackages = with pkgs; [
+    cbatticon
+    compton
+    emacs
+    git
+    go
+    hexchat
+    pcmanfm
+    python3
+    spotify
+    throttled
+    scrot
+    vim
+
+
+    networkmanager
+
+
+    arc-icon-theme
+    gnome3.gnome_terminal
+
+    shared_mime_info
+    desktop_file_utils
+
+    zsh
+    fish
+  ];
+
+
+  networking.networkmanager.enable = true;
+  services.throttled.enable = true;
+
+  # Enable sound.
+  sound.enable = true;
+  hardware.pulseaudio.enable = true;
+
+  services.xserver.enable = true;
+  services.xserver.layout = "us";
+  # services.xserver.xkbOptions = "eurosign:e";
+
+  # Enable touchpad support.
+  services.xserver.libinput.enable = true;
+
+  services.xserver.windowManager.xmonad.enable = true;
+  services.xserver.windowManager.awesome.enable = true;
+
+  environment.etc."xdg/gtk-3.0/settings.ini" =
+    {
+      text =
+        ''
+            [Settings]
+            gtk-icon-theme-name=Arc
+            gtk-theme-name=Arc
+        '';
+    };
+
+  users.users.eldritch = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ];
+  };
+  users.users.rski = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ];
+  };
+
+  # This value determines the NixOS release with which your system is to be
+  # compatible, in order to avoid breaking some software such as database
+  # servers. You should change this only after NixOS release notes say you
+  # should.
+  system.stateVersion = "19.09"; # Did you read the comment?
+  users.users.root.initialHashedPassword = "";
+
+}
+
---
+++ hardware-configuration.nix
@@ -0,0 +1,32 @@
+{ config, lib, pkgs, ... }:
+
+{
+  imports =
+    [ <nixpkgs/nixos/modules/installer/scan/not-detected.nix>
+    ];
+
+  boot.initrd.availableKernelModules = [ "nvme" "usb_storage" ];
+  boot.initrd.kernelModules = [ "dm-snapshot" ];
+  boot.kernelModules = [ "kvm-intel" ];
+  boot.extraModulePackages = [ ];
+
+  boot.initrd.luks.devices.crypted.device = "/dev/disk/by-uuid/387ccc52-0b22-45c8-b959-ea91043765d9";
+
+  fileSystems."/boot" =
+   { device = "/dev/disk/by-uuid/128F-061A";
+      fsType = "vfat";
+    };
+
+  fileSystems."/" = 
+     {
+       device= "/dev/disk/by-uuid/0e72e56f-99c7-48b7-a9cf-7c4c0b17f1ba";
+       fsType = "ext4";
+     };
+
+  swapDevices =
+    [ { device = "/dev/disk/by-uuid/13225e6f-38f3-4d60-b0e7-d281125d8c40"; }
+    ];
+
+  nix.maxJobs = lib.mkDefault 4;
+  powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
+}
